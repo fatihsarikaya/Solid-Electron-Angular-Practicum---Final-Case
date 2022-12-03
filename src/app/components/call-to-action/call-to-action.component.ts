afaa662f8a692ee9061e02798856cd8af3dd0cf7
Fatih Sarikaya <fatihsarikaya4@hotmail.com>
2022-12-03 21:36:34 +0300

    Add files via upload

---
+++ src/app/components/call-to-action/call-to-action.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-call-to-action',
+  templateUrl: './call-to-action.component.html',
+  styleUrls: ['./call-to-action.component.css']
+})
+export class CallToActionComponent implements OnInit {
+
+  bannerImg="https://images.unsplash.com/photo-1522158073024-fb5d4b4ccfdf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=840&q=80"
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
